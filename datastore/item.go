143f031692d17c77175c3a6be57c3bba90dd88c7
Omar Qazi <omar@predpol.com>
2015-06-25 04:07:10 -0700

    Added PlaylitstItem struct

---
+++ datastore/item.go
@@ -0,0 +1,10 @@
+package datastore
+
+// struct PlaylistItem describes a video playback instruction
+// in a channel's playback queue
+type PlaylistItem struct {
+	URLFormat string // The format for the url, with sequence number as argument
+	StartAt   int64  // The sequence number to start playing at
+	EndAt     int64  // The sequence number to stop playing at
+	Loop      bool
+}
