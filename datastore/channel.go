975bbe650d86dcbd51dab4c175d66866f4eaf9d7
Omar Qazi <omar@predpol.com>
2015-06-25 04:42:18 -0700

    added methods for advancing and resetting counter

--- datastore/channel.go
+++ datastore/channel.go
@@ -1,5 +1,7 @@
 package datastore
 
+import "strconv"
+
 // struct Channel describes a broadcast channel
 type Channel struct {
 	Identifier      string // Identifier is the unique part of the redis key
@@ -10,6 +12,20 @@ func GetChannel(channelId string) (rv Channel, err error) {
 	rv.Identifier = channelId
 	rv.PlaybackCounter, err = client.IncrBy(rv.PlaybackCounterKey(), 0).Result()
 	return
+}
+
+func (c *Channel) AdvanceCounter() error {
+	c.PlaybackCounter = c.PlaybackCounter + 1
+	return c.SaveCounter()
+}
+
+func (c *Channel) ResetCounter() error {
+	c.PlaybackCounter = 0
+	return c.SaveCounter()
+}
+
+func (c Channel) SaveCounter() error {
+	return client.Set(c.PlaybackCounterKey(), strconv.FormatInt(c.PlaybackCounter, 10), 0).Err()
 }
 
 func (c Channel) PlaybackCounterKey() string {
--- datastore/channel_test.go
+++ datastore/channel_test.go
@@ -11,3 +11,26 @@ func TestGetChannel(t *testing.T) {
 		t.Fatal("Error: expected playback counter of 0 but got", channel.PlaybackCounter)
 	}
 }
+
+const incChannel = "increment-channel"
+
+func TestMoveCounter(t *testing.T) {
+	channel, err := GetChannel(incChannel)
+	if err != nil {
+		t.Fatal("Error getting channel:", err)
+	}
+
+	initialValue := channel.PlaybackCounter
+	expectedValue := initialValue + 1
+	if err := channel.AdvanceCounter(); err != nil {
+		t.Fatal("Error advancing channel:", err)
+	} else if nchan, _ := GetChannel(incChannel); nchan.PlaybackCounter != expectedValue {
+		t.Error("Error: After incrementing channel expected value of", expectedValue, "but got", nchan.PlaybackCounter)
+	}
+
+	if err := channel.ResetCounter(); err != nil {
+		t.Fatal("Error reseting counter:", err)
+	} else if rchan, _ := GetChannel(incChannel); rchan.PlaybackCounter != 0 {
+		t.Error("Error: After reseting counter expected value of 0 but got", rchan.PlaybackCounter)
+	}
+}
